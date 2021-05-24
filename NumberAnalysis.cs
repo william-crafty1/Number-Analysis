5ec2a609aeafd7f0e97b58a85f5618d34b5cc281
Craft <william.craft@accenture.com>
2021-05-24 10:01:56 -0400

    intial loop and prompt

---
+++ NumberAnalysis.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Number_Analyzer
+{
+    class NumberAnalysis
+    {
+        static void Main(string[] args)
+        {
+            // Initialize variables
+            double number;
+            string choice;
+            Boolean loopCondtional = false;
+
+            // Main program loop.
+
+            while (loopCondtional != true)
+            {
+                Console.WriteLine("Please enter a number between 1 and 100.");
+                number = Convert.ToDouble(Console.ReadLine());
+
+                if (number > 0 && number < 100)
+                {
+                    loopCondtional = true;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a number between 1 and 100.");
+                }
+
+            }
+        }
+    }
+}
